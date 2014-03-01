2f231d3cd18ab1a5591c17cb638078cd50fd5e91
hfaran <hamzafaran@hotmail.com>
2014-03-01 00:20:52 -0800

    Move license to a file

--- proxy.py
+++ proxy.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python
-#
-# HTTP Proxy Server for Proxying to Local Virtual Hosts
-#
-# GET/POST proxying based on
-# http://groups.google.com/group/python-tornado/msg/7bea08e7a049cf26
-#
-# Copyright (C) 2012 Senko Rasic <senko.rasic@dobarkod.hr>
-# Copyright (C) 2014 Hamza Faran <hamza@hfaran.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+
+"""HTTP Proxy Server for Proxying to Local Virtual Hosts"""
+
 
 import random
 import json
@@ -35,8 +12,6 @@ import tornado.ioloop
 import tornado.iostream
 import tornado.web
 import tornado.httpclient
-
-__all__ = ['ProxyHandler', 'run_proxy']
 
 
 class ProxyHandler(tornado.web.RequestHandler):
